058730ac435904a4b2356f19938d63db661d156f
Timur Olzhabayev <timur.olzhabayev@grafana.com>
2024-05-23 12:51:13 +0200

    Testing lefthook

--- src/lib/buildStatusMetricProps.ts
+++ src/lib/buildStatusMetricProps.ts
@@ -63,7 +63,7 @@ export function buildStatusMetricProps(
         if ((warn <= crit && crit <= value) || (warn >= crit && crit >= value)) {
           fieldStatus = 'crit';
         } else if ((warn <= value && value <= crit) || (warn >= value && value >= crit)) {
-          fieldStatus = 'warn';       
+          fieldStatus = 'warn';
         }
 
         if (!_.isFinite(value)) {
