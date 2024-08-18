eb7cbe9b6e8661518f631c21500f89f679d62d71
BenjaminFourmaux <benjamin.fourmauxb@gmail.com>
2024-08-18 02:13:18 +0200

    update test accordingly

--- tests/panel.spec.ts
+++ tests/panel.spec.ts
@@ -2,6 +2,6 @@ import { expect, test } from '@grafana/plugin-e2e';
 
 test('should display Base severity', async ({ gotoPanelEditPage, readProvisionedDashboard }) => {
   const dashboard = await readProvisionedDashboard({ fileName: 'dashboard.json' });
-  const panelEditPage = await gotoPanelEditPage({ dashboard, id: '3' });
-  await expect(panelEditPage.panel.locator).toContainText('Base');
+  const panelEditPage = await gotoPanelEditPage({ dashboard, id: '2' });
+  await expect(panelEditPage.panel.locator).toContainText('success');
 });
